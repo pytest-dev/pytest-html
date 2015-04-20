c55660271f6dcf1c6aa9936db3857b0b8f66ca7f
Dave Hunt <dave.hunt@gmail.com>
2015-04-20 14:09:29 +0100

    Add missing extras module

---
+++ html/extras.py
@@ -0,0 +1,28 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+FORMAT_HTML = 'html'
+FORMAT_IMAGE = 'image'
+FORMAT_TEXT = 'text'
+FORMAT_URL = 'url'
+
+
+def extra(content, format, name=None):
+    return {'name': name, 'format': format, 'content': content}
+
+
+def html(content):
+    return extra(content, FORMAT_HTML)
+
+
+def image(content, name='Image'):
+    return extra(content, FORMAT_IMAGE, name)
+
+
+def text(content, name='Text'):
+    return extra(content, FORMAT_TEXT, name)
+
+
+def url(content, name='URL'):
+    return extra(content, FORMAT_URL, name)

b54c8885c9c74f5856f08c505d8b3d92a14d28ff
Florian Bruhin <git@the-compiler.org>
2015-08-11 22:23:53 +0200

    Add PyPI Trove classifiers for Python 3.

--- setup.py
+++ setup.py
@@ -25,4 +25,7 @@ setup(name='pytest-html',
           'Topic :: Utilities',
           'Programming Language :: Python',
           'Programming Language :: Python :: 2.6',
-          'Programming Language :: Python :: 2.7'])
+          'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.2',
+          'Programming Language :: Python :: 3.3',
+          'Programming Language :: Python :: 3.4'])

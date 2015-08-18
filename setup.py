947b036dc690fdb2636845343d185f667d5a6f47
Dave Hunt <dave.hunt@gmail.com>
2015-08-18 15:57:01 +0100

    Bump version number to 1.5.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytest-html',
-      version='1.5',
+      version='1.5.1',
       description='pytest plugin for generating HTML reports',
       long_description=open('README.rst').read(),
       author='Dave Hunt',

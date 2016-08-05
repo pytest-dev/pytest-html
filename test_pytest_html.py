1fe02f63dadf6ba439726e2c401e090b46ed0f94
Ana Ribeiro <aninhacostaribeiro@gmail.com>
2016-08-05 11:09:53 -0300

    Added excluded tests

--- test_pytest_html.py
+++ test_pytest_html.py
@@ -286,6 +286,44 @@ class TestHTML:
             href)
         assert link in html
 
+    def test_extra_url(self, testdir):
+        content = str(random.random())
+        testdir.makeconftest("""
+            import pytest
+            @pytest.mark.hookwrapper
+            def pytest_runtest_makereport(item, call):
+                outcome = yield
+                report = outcome.get_result()
+                if report.when == 'call':
+                    from pytest_html import extras
+                    report.extra = [extras.url('{0}')]
+        """.format(content))
+        testdir.makepyfile('def test_pass(): pass')
+        result, html = run(testdir)
+        assert result.ret == 0
+        link = '<a class="url" href="{0}" target="_blank">URL</a>'.format(
+            content)
+        assert link in html
+
+    def test_extra_image(self, testdir):
+        content = str(random.random())
+        testdir.makeconftest("""
+            import pytest
+            @pytest.mark.hookwrapper
+            def pytest_runtest_makereport(item, call):
+                outcome = yield
+                report = outcome.get_result()
+                if report.when == 'call':
+                    from pytest_html import extras
+                    report.extra = [extras.image('{0}')]
+        """.format(content))
+        testdir.makepyfile('def test_pass(): pass')
+        result, html = run(testdir, 'report.html', '--self-contained-html')
+        assert result.ret == 0
+        assert '<a class="image" href="#" target="_blank">Image</a>' in html
+        src = 'data:image/png;base64,{0}'.format(content)
+        assert '<a href="#"><img src="{0}"/></a>'.format(src) in html
+
     @pytest.mark.parametrize('file_extension,file_type',
                              [('png', 'image'),
                               ('json', 'json'),

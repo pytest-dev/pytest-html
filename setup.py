5ca382751e769d52a5b6e1d9267c2e21b42d30a1
Dave Hunt <dave.hunt@gmail.com>
2016-08-09 15:07:22 -0700

    Bumped version number to 1.10.0 and updated release notes

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytest-html',
-      version='1.9.0',
+      version='1.10.0',
       description='pytest plugin for generating HTML reports',
       long_description=open('README.rst').read(),
       author='Dave Hunt',

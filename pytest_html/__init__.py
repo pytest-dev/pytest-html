fba7e3907e8af985e4d81d40da87529a9e4342bf
Dave Hunt <dave.hunt@gmail.com>
2016-11-30 09:15:41 +0000

    Don't raise if package is not installed

--- pytest_html/__init__.py
+++ pytest_html/__init__.py
@@ -5,6 +5,6 @@ try:
     __version__ = get_distribution(__name__).version
 except DistributionNotFound:
     # package is not installed
-    raise
+    pass
 
 __pypi_url__ = 'https://pypi.python.org/pypi/pytest-html'

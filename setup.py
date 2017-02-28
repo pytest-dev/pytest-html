c6fc62c5501e6ef6765b3e6d100e03cd7b1d5cb7
Dave Hunt <dave.hunt@gmail.com>
2017-02-28 08:43:35 +0000

    Adjust classifiers to indicate Python version support

--- setup.py
+++ setup.py
@@ -28,9 +28,5 @@ setup(name='pytest-html',
           'Topic :: Software Development :: Testing',
           'Topic :: Utilities',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
-          'Programming Language :: Python :: 3.2',
-          'Programming Language :: Python :: 3.3',
-          'Programming Language :: Python :: 3.4',
-          'Programming Language :: Python :: 3.5'])
+          'Programming Language :: Python :: 3.6'])

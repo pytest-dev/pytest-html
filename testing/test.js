13a299f28fee5e0c3e7fef57f78ac24d2bae0528
Jace Browning <jacebrowning@gmail.com>
2018-05-31 12:46:31 -0400

    Add implementation of 'includes' for PhantomJS
    https://stackoverflow.com/a/31361163/429533

--- testing/test.js
+++ testing/test.js
@@ -2,6 +2,12 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+ if (!String.prototype.includes) {
+   String.prototype.includes = function() {'use strict';
+     return String.prototype.indexOf.apply(this, arguments) !== -1;
+   };
+ }
+
  QUnit.module( 'module', {
    beforeEach: function( assert ) {
       init();

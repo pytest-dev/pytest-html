bf286c14dc916012a9e1553d44aff4d619f48850
SunInJuly <sun_in_july@mail.ru>
2019-02-18 20:29:07 +0300

    more Flake8 issues fixed

--- testing/test_pytest_html.py
+++ testing/test_pytest_html.py
@@ -416,9 +416,9 @@ class TestHTML:
         hash_generator = hashlib.md5()
         hash_generator.update(hash_key.encode('utf-8'))
         assert result.ret == 0
-        src = '{0}/{1}'.format('assets', '{0}_{1}.{2}'.format(hash_key,
-                                                              hash_generator.hexdigest(),
-                                                              file_extension))
+        src = '{0}/{1}'.format('assets', '{0}_{1}.{2}'.
+                               format(hash_key, hash_generator.hexdigest(),
+                                      file_extension))
         link = ('<a class="image" href="{0}" target="_blank">'.format(src))
         assert link in html
         assert os.path.exists(src)

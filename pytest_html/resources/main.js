274935bad548503421685508bf7339c9e6c3e557
Wanam <wanam@users.noreply.github.com>
2019-07-11 17:14:42 +0200

    Keep sort preference for previously sorted columns

--- pytest_html/resources/main.js
+++ pytest_html/resources/main.js
@@ -144,12 +144,15 @@ function sort(items, key_func, reversed) {
     var sort_array = items.map(function(item, i) {
         return [key_func(item), i];
     });
-    var multiplier = reversed ? -1 : 1;
 
     sort_array.sort(function(a, b) {
         var key_a = a[0];
         var key_b = b[0];
-        return multiplier * (key_a >= key_b ? 1 : -1);
+        if (reversed) {
+            return (key_a < key_b ? 1 : -1);
+        } else {
+            return (key_a > key_b ? 1 : -1);
+        }
     });
 
     return sort_array.map(function(item) {

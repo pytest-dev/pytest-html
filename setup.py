4d93ca20f9b7a40be975523b8e688673df687ed0
Jim Brännlund <jimbrannlund@fastmail.com>
2022-02-04 23:46:50 +0100

    troubleshoot failing windows test

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     package_data={"pytest_html": ["resources/*"]},
     entry_points={"pytest11": ["html = pytest_html.plugin"]},
     setup_requires=["setuptools_scm"],
-    install_requires=["pytest>=5.0,!=6.0.0", "pytest-metadata"],
+    install_requires=["pytest>=5.0,!=6.0.0,<7.0.0", "pytest-metadata"],
     license="Mozilla Public License 2.0 (MPL 2.0)",
     keywords="py.test pytest html report",
     python_requires=">=3.6",

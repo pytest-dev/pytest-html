142bd4a6082988cc4a61ed7ea4f849fcfb0ee405
Jim Brännlund <jim.brannlund@gmail.com>
2023-04-02 21:18:21 +0200

    chore: Migrate from Poetry to Hatch (#617)

--- Gruntfile.js
+++
@@ -1,9 +0,0 @@
-module.exports = function(grunt) {
-  grunt.initConfig({
-    qunit: {
-      src: 'testing/js_test_report.html'
-    }
-  });
-  grunt.loadNpmTasks('grunt-contrib-qunit');
-  grunt.registerTask('test', 'qunit:src');
-};

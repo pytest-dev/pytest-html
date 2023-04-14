8287c84588cd4db0b2cdddfbf6cbd28712942ae4
Jim Brännlund <jim.brannlund@gmail.com>
2023-04-14 15:01:48 +0200

    Chore: Add npm build hooks (#649)

---
+++ scripts/npm.py
@@ -0,0 +1,9 @@
+import subprocess
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+class NpmBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        subprocess.check_output("npm install", shell=True)
+        subprocess.check_output("npm run build", shell=True)

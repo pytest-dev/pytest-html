3a41ff6160ce193fe989dead8803ca343671774b
Jim Brännlund <jim.brannlund@gmail.com>
2023-04-15 11:38:03 +0200

    Chore: Fix npm building

--- scripts/npm.py
+++ scripts/npm.py
@@ -5,5 +5,5 @@ from hatchling.builders.hooks.plugin.interface import BuildHookInterface
 
 class NpmBuildHook(BuildHookInterface):
     def initialize(self, version, build_data):
-        subprocess.check_output("npm install", shell=True)
+        subprocess.check_output("npm ci", shell=True)
         subprocess.check_output("npm run build", shell=True)

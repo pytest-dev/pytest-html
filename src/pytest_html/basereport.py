d0ca2d80fc6ff03f13a3053d9233f11328dc133a
Adrien Berchet <adrien.berchet@gmail.com>
2023-09-13 09:12:13 +0200

    Use resolve() instead of absolute()

--- src/pytest_html/basereport.py
+++ src/pytest_html/basereport.py
@@ -18,9 +18,7 @@ from pytest_html.util import cleanup_unserializable
 
 class BaseReport:
     def __init__(self, report_path, config, report_data, template, css):
-        self._report_path = (
-            Path(os.path.expandvars(report_path)).expanduser().absolute()
-        )
+        self._report_path = Path(os.path.expandvars(report_path)).expanduser().resolve()
         self._report_path.parent.mkdir(parents=True, exist_ok=True)
         self._config = config
         self._template = template
@@ -188,7 +186,7 @@ class BaseReport:
     def pytest_terminal_summary(self, terminalreporter):
         terminalreporter.write_sep(
             "-",
-            f"Generated html report: file://{self._report_path.resolve().as_posix()}",
+            f"Generated html report: file://{self._report_path.as_posix()}",
         )
 
     @pytest.hookimpl(trylast=True)

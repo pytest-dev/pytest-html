f195a66bd143119aa3d81e30f5a4546eccfeb66d
Pierre Sassoulas <pierre.sassoulas@gmail.com>
2025-02-04 17:44:03 +0100

    eslint upgrade ?

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,44 @@
-{
-    "env": {
-        "browser": true,
-        "commonjs": true,
-        "es2021": true
+import globals from "globals";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
+import js from "@eslint/js";
+import { FlatCompat } from "@eslint/eslintrc";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({
+    baseDirectory: __dirname,
+    recommendedConfig: js.configs.recommended,
+    allConfig: js.configs.all
+});
+
+export default [...compat.extends("google"), {
+    languageOptions: {
+        globals: {
+            ...globals.browser,
+            ...globals.commonjs,
+        },
+        ecmaVersion: "latest",
+        sourceType: "script",
     },
-    "extends": [
-        "google"
-    ],
-    "parserOptions": {
-        "ecmaVersion": "latest"
-    },
-    "rules": {
+
+    rules: {
         "array-bracket-spacing": "error",
         "block-scoped-var": "error",
         "block-spacing": "error",
         "brace-style": "error",
-        "camelcase": "off",
+        camelcase: "off",
         "class-methods-use-this": "error",
         "consistent-return": "error",
         "default-case": "error",
         "default-case-last": "error",
         "default-param-last": "error",
         "grouped-accessor-pairs": "error",
-        "indent": [ "error", 4 ],
-        "linebreak-style": [ "error", "unix" ],
-        "max-len": ["error", { "code": 120 }],
+        indent: ["error", 4],
+        "linebreak-style": ["error", "unix"],
+        "max-len": ["error", {
+            code: 120,
+        }],
         "no-caller": "error",
         "no-console": "error",
         "no-empty-function": "error",
@@ -45,26 +59,16 @@
         "no-useless-backreference": "error",
         "no-useless-concat": "error",
         "no-var": "error",
-        "object-curly-spacing": [
-            "error",
-            "always",
-            {
-                "arraysInObjects": true
-            }
-        ],
+        "object-curly-spacing": ["error", "always", {
+            arraysInObjects: true,
+        }],
         "prefer-const": "error",
         "prefer-promise-reject-errors": "error",
         "require-atomic-updates": "error",
         "require-await": "error",
-        "require-jsdoc" : 0,
-        "semi": [
-            "error",
-            "never"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "yoda": "error"
-    }
-}
+        "require-jsdoc": 0,
+        semi: ["error", "never"],
+        quotes: ["error", "single"],
+        yoda: "error",
+    },
+}];
